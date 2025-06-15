925c843d8b6a7bfc22e2ca4d3f2689338c805e12
Wyatt Puff <puffwya000@gmail.com>
2025-06-15 13:07:05 -0400

    Fixed form field name

--- calorieapp/forms.py
+++ calorieapp/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class FoodSearchForm(forms.Form):
     query = forms.CharField(label='Food name', max_length=100)
-    food_name = form.cleaned_data['query']
+    food_name = forms.cleaned_data['query']
