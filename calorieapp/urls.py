45caaff17fb33c0868c43f73353a211ecfb507f5
Wyatt Puff <puffwya000@gmail.com>
2025-06-23 14:09:03 -0400

    Added admin to urls.py

--- calorieapp/urls.py
+++ calorieapp/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.http import HttpResponse
 
@@ -23,6 +23,6 @@ def home(request):
     return HttpResponse("Hello! Your Django app is running.")
 
 urlpatterns = [
-    # path('admin/', admin.sit
+    path('admin/', admin.site.urls),
     path('', views.food_search, name='food_search'),
 ]
